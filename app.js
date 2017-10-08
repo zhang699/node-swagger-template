d48cf3fee2ea2d6b4637b1191ef9840f38a44cef
jimmy.jhang <t6820310@ntut.org.tw>
2017-10-08 13:53:42 +0800

    change to es6 syntax

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
-"use strict";
-
-var SwaggerExpress = require("swagger-express-mw");
-var express = require("express");
-var cors = require("cors");
-var app = express();
-module.exports = app; // for testing
-
+import SwaggerExpress from "swagger-express-mw";
+import express from "express";
+import cors from "cors";
+const app = express();
 app.use(cors());
+
 /** connect to mongodb */
-require("./api/helpers/mongodb");
+import "./api/helpers/mongodb";
 
 const TOKEN = "Ss-^3EnbsM`Mbp(#ou2})&wXYn|Pu";
-var config = {
+const config = {
   appRoot: __dirname, // required config
   swaggerSecurityHandlers: {
     Bearer: function(req, authOrSecDef, scopesOrApiKey, callback) {
@@ -34,6 +31,8 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   // install middleware
   swaggerExpress.register(app);
 
-  var port = process.env.PORT || 10010;
+  const port = process.env.PORT || 10010;
   app.listen(port);
 });
+
+export default app;
